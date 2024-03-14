85c6dda9fb0272c791a627f0bc94c3940479f705
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-03-14 07:11:30 +0500

    javascript code data type,varaible,math expressions,conditional operator

---
+++ 13-03-2024/13-03-2024/index.js
@@ -0,0 +1,44 @@
+
+// var userData = prompt("enter something")
+
+// if (!userData) {
+//     alert("your data" + userData)
+// } else if (Number(userData)) {
+//     alert("Number data " + userData)
+// } else {
+//     alert("wrong data")
+// }
+
+// var userAge = +prompt("enter your age")
+
+// if (userAge <= 18) {
+//     console.log("you are under age for this program")
+// } else if (userAge >= 50) {
+//     console.log("you are over age for this program")
+// } else {
+//     console.log("you are eligible for this program")
+// }
+
+
+// var num1 = 12;
+// var num2 = 11;
+// var num3 = true;
+// var num4 = "helloo";
+
+// if (num1 && num2 && !num3 && num4) {
+//     console.log("this is a number")
+// } else {
+//     console.log("this is not a number")
+// }
+
+// var age = 12;
+// var res = "U.S.";
+
+// if (age > 65 || age < 21 && res === "U.S.") {
+//     console.log("this is a number")
+// } else {
+//     console.log("first")
+// }
+
+
+
---
+++ 6-03-2024/6-03-2024/index.js
@@ -0,0 +1,42 @@
+// Math expressions:
+// Unfamiliar operators
+
+// num++;
+// num = num + 1
+
+// var num = 1;
+// var newNum = ++num;
+
+// var num = 1;
+// var newNum = num--;
+
+// console.log(newNum)
+
+// var totalCost = 1 + (3 * 4);
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+
+// var resultOfComputation = ((2 * 4) * 4) + 2;
+
+// var resultOfComputation = (2 * 4) * (4 + 2)
+// console.log(resultOfComputation)
+
+// var message = "Thanks for your input!";
+
+// var userName = prompt("enter your name..")
+// alert("Thanks, " + userName + " welcome");
+
+
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+// var age = +prompt("enter your age")
+
+// if (age < 18) {
+//     alert("your are underage")
+// } else {
+//     alert("your are adult")
+// }
