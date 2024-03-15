97bb298d545ee9e3eb24e71cf6d8edcd75c3d936
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-03-16 01:09:54 +0500

    basic of dom

---
+++ 15-03-2024/index.js
@@ -0,0 +1,78 @@
+// var headTag = document.getElementById("heading");
+// // headTag.style.color = "green"
+
+// var headingTag = document.getElementsByClassName("headingTitle");
+// // headingTag[1].style.color = "red"
+// // console.log(headingTag[0])
+
+// //target id
+// var head = document.querySelector("#userName")
+// //target class
+// var head = document.querySelector("۔headingTitle")
+// //target first tag
+// var head = document.querySelector("h1")
+
+
+// var headingsAll = document.querySelectorAll("h1")
+// console.log(headingsAll, "headingsAll")
+
+
+// var btn = document.getElementById("btn")
+// console.log(btn)
+
+
+// var userData = prompt("Enter any fruit name")
+
+// switch (userData) {
+//     case "Oranges":
+//         console.log("Oranges are $0.59 a pound.");
+//         break;
+//     case "Apples":
+//         console.log("Apples are $0.32 a pound.");
+//         break;
+//     case "Bananas":
+//         console.log("Bananas are $0.48 a pound.");
+//         break;
+//     case "Cherries":
+//         console.log("Cherries are $3.00 a pound.");
+//         break;
+//     case "Mangoes":
+//     case "Papayas":
+//         console.log("Mangoes and papayas are $2.79 a pound.");
+//         break;
+//     default:
+//         console.log(`Sorry, we are out of.`);
+// }
+
+// console.log("Is there anything else you'd like?");
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// var userData = +prompt("Enter your age")
+
+// switch (userData) {
+//     case (userData > 18):
+//         console.log("you are eligible for this program");
+//         break;
+//     case (userData < 18):
+//         console.log("you are not eligible for this program");
+//         break;
+//     case (userData < 10):
+//         console.log("to young for this program");
+//         break;
+//     case (userData > 50):
+//         console.log("your age is not suitable for this program");
+//         break;
+//     case (userData > 100):
+//         console.log("yuor time for rest and peace");
+//         break;
+//     default:
+// console.log("Sorry, we are out of.");
+// }
