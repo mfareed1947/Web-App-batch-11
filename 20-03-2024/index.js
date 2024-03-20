b2f45edf252dff119cbbc432044921c9ce84ec5c
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-03-20 09:54:57 +0500

    arrays start

---
+++ 20-03-2024/index.js
@@ -0,0 +1,26 @@
+
+// var userData = +prompt("Enter a number");
+
+// if (userData > 10) {
+//     var result = userData * 8
+//     if (result < 100) {
+//         console.log(result, "result")
+//     } else {
+//         console.log(result,"World")
+//     }
+// }else{
+//     console.log("user input is less than 10")
+// }
+
+// Arrays
+
+// var newArr = ["ali", 23, true]
+// console.log(newArr[0])
+
+// var userInput = prompt("Enter any fruit name")
+// var fruitsName = ["mango", "banana", "apples", "orange", "watermellon"]
+// // fruitsName.push(userInput)
+// // fruitsName.pop(userInput)
+// // console.log(fruitsName)
+// console.log(fruitsName[userInput])
+
