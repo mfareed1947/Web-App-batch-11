37c9134aa74997d1bbfe5c5788e9364875c95377
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-03-26 23:27:47 +0500

    add 22 and 25 date javascript code

---
+++ 22-03-2024/index.js
@@ -0,0 +1,19 @@
+// // Arrays
+
+// var newArr = ["ali", "raza", "ahmed", "maaz", "john", "hamza", "haris"]
+// // var nestedArr = ["yasir", "umer", "bilal"]
+
+// // newArr.push("yasir", "umer", "bilal")
+// //  add last element in Array
+
+// // newArr.pop() remove last element in Array
+
+// // newArr.shift() remove first element in Array
+
+// // newArr.unshift("yasir")
+// // add first element in Arrays
+
+
+// // newArr.splice(-3, 1, "wasi")
+// newArr.splice(1, 2)
+// console.log(newArr)
---
+++ 25-03-2024/index.js
@@ -0,0 +1,17 @@
+// var newArr = ["dog", "cat", "fly", "bug", "bird", "fish"]
+
+// var arr = newArr.slice(-1, -4)
+
+// console.log(arr, "newArr")
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+var newArr = ["ali", "ahmed", "raza"]
+
+for (var index = 0; index < newArr.length; index++) {
+    var result = newArr[index]
+    console.log(index, result)
+}
