4daa603fa7a8528dc7bc41d6b2b1e5a24e45c190
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-03-27 08:46:52 +0500

    for loop with conditional or nested for loop

---
+++ 27-03-2024/index.js
@@ -0,0 +1,28 @@
+// var pets = ["dog", "cat", "horse", "birds"];
+// // var userInput = prompt("Enter  any pet name")
+
+// // for (let i = 0; i < pets.length; i++) {
+// //     if (userInput === pets[i]) {
+// //         // console.log(userInput === pets[i])
+// //         console.log("pets found")
+// //         break;
+// //     } else {
+// //         // console.log(userInput === pets[i])
+// //         console.log("pets not found")
+// //     }
+// // }
+
+// // for (let i = 0; i < 5; i++) {
+// //     // for (let j = 0; j < 5; j++) {
+// //         document.write("hello world")
+// //     // }
+// // }
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+// for (var i = 0; i < firstNames.length; i++) {
+//     for (var j = 0; j <= i; j++) {
+//         fullNames.push(firstNames[i] + lastNames[j]);
+//     }
+// }
+// console.log(fullNames)
