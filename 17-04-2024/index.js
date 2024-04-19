51a7e460edf53ceb201090dd25bcf136606aeea6
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-04-19 11:36:11 +0500

    javascript string methods

---
+++ 17-04-2024/index.js
@@ -0,0 +1,41 @@
+// var paragraph = "hello is world hello";
+
+// var firstChar = paragraph.indexOf("o", 5);
+
+// console.log(firstChar, "firstChar")
+
+
+
+// var paragraph = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+// var firstChar = paragraph.lastIndexOf("and scrambled");
+
+// console.log(firstChar, "firstChar")
+
+
+
+// var firstName = "qwerty"
+
+// var firstChar = firstName.charAt(firstName.length - 1)
+// console.log(firstChar, "firstChar")
+
+
+// var userName = "fareed";
+
+// var capp = userName.charAt(0).toUpperCase() + userName.slice(1)
+// console.log(capp, "capp")
+
+
+// var paragraph = "I think Ruth's dog is cute than your dog!";
+
+// console.log(paragraph.replace("Ruth's", 'my'));
+
+
+// var str = 'The quick brown | fox jumps over the lazy dog.';
+
+// console.log(str.split(""))
+// console.log(str.split("|"))
+
+// var sentence = 'The quick brown fox 111 jumps overs, the lazy dog.';
+
+// console.log(sentence.includes("111"))
