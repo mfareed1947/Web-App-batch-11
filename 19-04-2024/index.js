6dbe810e6b184c570556e908398503f374f66feb
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-04-20 22:56:07 +0500

    function javascript

---
+++ 19-04-2024/index.js
@@ -0,0 +1,39 @@
+// function addition(num1, num2) {
+//     var ans = num1 + num2;
+//     return ans;
+// }
+
+// // var result = addition(4, 7)
+// console.log(addition(4, 7), "result")
+
+
+// var res = function (pas) {
+//     return pas;
+// }
+
+// console.log(res(12))
+
+// function calculate(num1, num2, operator) {
+//     if (operator === 1) {
+//         return num1 + num2;
+//     } else if (operator === 2) {
+//         return num1 - num2;
+//     }
+//     else if (operator === 3) {
+//         return num1 * num2;
+//     }
+//     else if (operator === 4) {
+//         return num1 / num2;
+//     }
+//     else {
+//         alert("This is not a operator")
+//     }
+// }
+// var num1Input = +prompt("enter number1")
+// var num2Input = +prompt("enter number2")
+// var operators = +prompt("enter a operators \n + press 1 \n - press 2 \n * press 3 \n / press 4")
+
+
+// var res = calculate(num1Input, num2Input, operators);
+// console.log(calculate(num1Input, num2Input, operators))
+
