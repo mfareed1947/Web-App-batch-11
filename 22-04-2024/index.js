8ef5154a392434840cab9251c6c5a4d433a28f22
Sarmed Rizvi <mfareed1504@gmail.com>
2024-04-22 23:54:11 +0500

    JavaScript 25 to 30 chapters

---
+++ 22-04-2024/index.js
@@ -0,0 +1,94 @@
+//*********************************/ MATH METHODS
+
+// console.log(Math.round(0.9));
+
+// console.log(Math.round(5.95), Math.round(5.4), Math.round(5.05));
+
+
+// console.log(Math.round(-5.05), Math.round(-5.5), Math.round(-5.95));
+
+// console.log(Math.round(-20.51));
+
+
+// console.log(Math.random() * 100)
+
+// var bigDecimal = Math.random();
+// var improvedNum = (bigDecimal * 6) + 1;
+// var numberOfStars = Math.floor(improvedNum);
+// console.log(numberOfStars)
+
+// console.log(Math.max(1, 3, 2));
+// console.log(Math.min(1, 3, 2));
+// var userName = "fareed";
+
+// function powerNumber(base, exponent) {
+//     // var userName = "asad";
+//     // console.log(userName, "inside func")
+//     return Math.pow(base, exponent)
+// }
+
+// var result = powerNumber(6, 3);
+// // var result2 = powerNumber(9, 5);
+// console.log(userName, "outside")
+// // console.log(result2)
+
+// var userInputBase = +prompt("Enter a base")
+// var userInputExp = +prompt("Enter a exponent")
+
+// var res = powerNumber(userInputBase, userInputExp)
+// console.log(res)
+
+// console.log(Math.floor(5.95));
+// // Expected output: 5
+
+// console.log(Math.floor(5.05));
+// // Expected output: 5
+
+// console.log(Math.floor(5));
+// // Expected output: 5
+
+// console.log(Math.floor(-5.05));
+// // Expected output: -6
+
+// console.log(Math.ceil(0.95));
+// // Expected output: 1
+
+// console.log(Math.ceil(4));
+// // Expected output: 4
+
+// console.log(Math.ceil(7.004));
+// // Expected output: 8
+
+// console.log(Math.ceil(-7.004));
+// // Expected output: -7
+
+//**************************** Converting strings to integers and decimals
+
+// var profit = "167.3" - "125.67";
+
+// var profit = "200" - "duck";
+// var result = "200" + 150;
+// var result = 150 + + "200";
+// var result = parseInt("780");
+// var result = parseFloat(780.879);
+// console.log(result)
+
+// function circumference(r) {
+//     return parseFloat(r) * 2.0 * Math.PI;
+// }
+// console.log(circumference(4.567))
+
+
+// var integerString = "24"
+// var num = Number(integerString);
+
+//****************************Converting strings to numbers, numbers to strings
+
+// var numberAsNumber = 1234;
+// var numberAsString = numberAsNumber.toString();
+// console.log(numberAsString)
+
+// var total = 42324.55242424234
+// var prettyTotal = total.toFixed(5);
+// console.log(Number(prettyTotal))
+
