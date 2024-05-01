700a91c89676e01c260dd4769e38960c648f4128
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-05-01 19:01:00 +0500

    array methods

---
+++ 1-05-24/1-05-24/index.js
@@ -0,0 +1,47 @@
+// const array1 = [2, 4, 6];
+// array1.forEach((x, i, a) => console.log(i, x * 2));
+// const array2 = [2, 4, 6];
+
+// const map1 = array2.map((elem) => elem * 2);
+// console.log(map1)
+// var hekko = (params = "asad") => console.log(params, "aa")
+// console.log(hekko("hamza"))
+
+
+
+// const array1 = ['one', 'two', 'three'];
+// console.log('array1:', array1);
+
+// const reversed = array1.reverse();
+// console.log('reversed:', reversed);
+
+
+
+
+
+/// SEARCH METHODS
+
+// const words = [400, 560, 324, 678, 213, 644, 961, 340];
+// const words = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+// const result = words.filter((num, i) => num % 2 !== 0);
+
+// console.log(result)
+
+// const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+// const result = words.filter((word) => word !== "elite");
+// console.log(result)
+
+// const array1 = [5, 12, 8, 130, 44];
+
+// const found = array1.find((element) => element > 10);
+
+// console.log(found)
+
+// const array1 = [1, 2, 3];
+
+// console.log(array1.includes(21));
+
+// const elements = ['Fire', 'Air', 'Water'];
+
+// console.log(elements.join(" & "));
