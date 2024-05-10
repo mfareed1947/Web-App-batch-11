72bd791a838e84869512de15d0e8028de7042c7f
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-05-10 10:40:38 +0500

    commit for streak

---
+++ 10-05-2024/10-05-2024/index.js
@@ -0,0 +1,10 @@
+// const userInput = document.getElementById("userInput")
+
+// userInput.addEventListener("focus", () => {
+//     console.log("hello world");
+// })
+// userInput.addEventListener("blur", () => {
+//     console.log("hello world");
+// })
+userInput.addEventListener('change', (e) => console.log(e.target.value))
+userInput.addEventListener('select', (e) => console.log(e.target.value))
