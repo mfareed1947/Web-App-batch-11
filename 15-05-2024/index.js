9c2f3295d293e26970e9df66c488bed3949f2c24
Sarmed Rizvi <81671003+mfareed1947@users.noreply.github.com>
2024-05-15 22:07:47 +0500

    chapter 51 to 54

---
+++ 15-05-2024/index.js
@@ -0,0 +1,28 @@
+// function expandLoris() {
+//     const expandedParagraph = "Hi, I am Muhammad Fareed";
+//     document.getElementById("slowLoris").innerHTML = expandedParagraph;
+// }
+
+// const testing = document.getElementById("slowLoris").innerHTML;
+// const testing = document.getElementById("slowLoris").innerText;
+// const testing = document.getElementById("slowLoris").outerHTML;
+// const testing = document.getElementById("slowLoris").outerText;
+
+// console.log(testing)
+
+// function placeAList() {
+//     var listToPlace = "<ol><li>Slow loris</li><li>Fast loris</li><li>Just-right loris</li></ol>";
+//     document.getElementById("slowLoris").innerText = listToPlace;
+
+// }
+
+const btn = document.getElementById("btn")
+
+// function makeInvisible() {
+//     document.getElementById("ugly").className = "gayb";
+// }
+btn.addEventListener("click", () => {
+    const img = document.getElementById("hello").classList;
+    img.toggle("dark-mode");
+})
+
