aeab281e4e208a69c197120c20d4406ab82a8e3b
Sarmed Rizvi <mfareed1504@gmail.com>
2024-05-20 03:22:14 +0500

    objects

--- 20-05-2024/index.js
+++ 20-05-2024/index.js
@@ -1,47 +1,46 @@
-// // const stdName = "umer";
+const stdName = "umer";
 
-// // const studentObj = {
-// //     id: "345-23",
-// //     stdName: "wasi",
-// //     course: "web and App",
-// //     location: "karachi",
-// //     getName: function (param) {
-// //         console.log(`Hi, I am ${param} ${this.stdName} ${this.course}`);
-// //     }
-// // }
-// // console.log(studentObj.getName("muhammad"))
+const studentObj = {
+  id: "345-23",
+  stdName: "wasi",
+  course: "web and App",
+  location: "karachi",
+  getName: function (param) {
+    console.log(`Hi, I am ${param} ${this.stdName} ${this.course}`);
+  },
+};
+console.log(studentObj.getName("muhammad"));
 
-// const plans = [
-//     {
-//         plan: "basic",
-//         monthly: 367,
-//         diskSpace: "100GB",
-//         dataTransfer: "500GB",
-//         sitePages: 10,
-//         discount: [45, 56, 47564],
-//         feature: true,
-//     },
-//     {
-//         plan: "professional",
-//         monthly: 234,
-//         diskSpace: "100GB",
-//         dataTransfer: "500GB",
-//         sitePages: 10,
-//         data: {
-//             age: 23,
-//             name: "Ahmed"
-//         }
-//     },
-//     {
-//         plan: "Ultimate",
-//         monthly: 429,
-//         diskSpace: "100GB",
-//         dataTransfer: "500GB",
-//         sitePages: 10,
+const plans = [
+  {
+    plan: "basic",
+    monthly: 367,
+    diskSpace: "100GB",
+    dataTransfer: "500GB",
+    sitePages: 10,
+    discount: [45, 56, 47564],
+    feature: true,
+  },
+  {
+    plan: "professional",
+    monthly: 234,
+    diskSpace: "100GB",
+    dataTransfer: "500GB",
+    sitePages: 10,
+    data: {
+      age: 23,
+      name: "Ahmed",
+    },
+  },
+  {
+    plan: "Ultimate",
+    monthly: 429,
+    diskSpace: "100GB",
+    dataTransfer: "500GB",
+    sitePages: 10,
+  },
+];
 
-//     }
-// ];
-
-// console.log(plans.filter((curr) => (curr.monthly > 300)))
-// console.log(plans[0].monthly)
-// console.log(plans[0].discount)
+console.log(plans.filter((curr) => curr.monthly > 300));
+console.log(plans[0].monthly);
+console.log(plans[0].discount);
